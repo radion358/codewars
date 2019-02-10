edfa12ca4634cb443e65e3b5470f7eb2a7534b7f
radion358 <radoslaw.dauksza@gmail.com>
2019-02-10 05:23:56 +0100

    solution

--- src/main/java/Highest_and_Lowest/Kata.java
+++ src/main/java/Highest_and_Lowest/Kata.java
@@ -1,8 +1,15 @@
 package Highest_and_Lowest;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Kata {
     static String HighAndLow(String numbers) {
-
-        return "throw towel";
+        List<String> splittedNumbers = Arrays.asList(numbers.split(" "));
+        int min = splittedNumbers.stream().mapToInt(s -> Integer.parseInt(s))
+                .min().orElseThrow(RuntimeException::new);
+        int max = splittedNumbers.stream().mapToInt(s -> Integer.parseInt(s))
+                .max().orElseThrow(RuntimeException::new);
+        return max + " " + min;
     }
 }
